834bbf2913c8118666a2664e6c212b1531225497
Birajdar <suraj.birajdar@harman.com>
2026-04-27 18:32:02 +0530

    updated clientApp file

--- tests/clientApp.spec.js
+++ tests/clientApp.spec.js
@@ -2,5 +2,10 @@ const {test, expect} = require('@playwright/test');
 
 test('First playwright test', async ({page})=>{
     await page.goto("https://rahulshettyacademy.com/client");   
+    await page.locator("#userEmail").fill("");
+    await page.locator("#userPassword").fill("");
+    await page.locator("#login").click();
+    await page.locator(".card-body b").first().waitFor();
+    console.log(await page.locator(".card-body b").allTextContents());
 }
 );
